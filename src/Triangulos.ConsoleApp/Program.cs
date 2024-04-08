642dc3ce6e0d877886458e3e7bd7c7baf1fc975f
MaCout0 <jkmbint@gmail.com>
2024-04-08 19:10:45 -0300

    Adicionando o código de classificação de triangulo com objeto

--- src/Triangulos.ConsoleApp/Program.cs
+++ src/Triangulos.ConsoleApp/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //declarar variavel de classe
+            Triangulo triangulo = new Triangulo();
+
+            Console.WriteLine("Passe os valores do triangulo: ");
+            Console.Write("X = ");
+            triangulo.x = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Y = ");
+            triangulo.y = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Z = ");
+            triangulo.z = Convert.ToDecimal(Console.ReadLine());
+
+            triangulo.VerificaTriangulo();
+
+            triangulo.ClassificaTriangulo();
+
+            Console.ReadLine ();
         }
     }
 }
---
+++ src/Triangulos.ConsoleApp/Triangulo.cs
@@ -0,0 +1,71 @@
+﻿namespace Triangulos.ConsoleApp
+{
+    public class Triangulo
+    {
+        //atributos
+        public decimal x;
+        public decimal y;
+        public decimal z;
+        public int count = 0;
+
+        public void VerificaTriangulo()
+        {
+            decimal[] somaLados = new decimal[3];
+            somaLados[0] = x + y;
+            somaLados[1] = y + z;
+            somaLados[2] = x + z;
+
+
+            for (int i = 0; i < somaLados.Length; i++)
+            {
+                if (somaLados[i] > x)
+                {
+                    count++;
+                }
+            }
+            for (int i = 0; i < somaLados.Length; i++)
+            {
+                if (somaLados[i] > y)
+                {
+                    count++;
+                }
+            }
+            for (int i = 0; i < somaLados.Length; i++)
+            {
+                if (somaLados[i] > z)
+                {
+                    count++;
+                }
+            }
+        }
+
+        public void ClassificaTriangulo()
+        {
+            if (count != 9)
+            {
+                Console.WriteLine("Os valores não compõe um triangulo.");
+            }
+            else
+            {
+                //classificando o triangulo
+                if (x == y && y == z)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Este triangulo é equilatero");
+                }
+                else if (x == y && y != z)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Esse triangulo é isósceles");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Esse triangulo é escaleno");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
